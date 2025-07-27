24f2aed6dc00c39e1f56dcb5d7403fc789f9c4f1
Patrick Okare <patrickbabawale@gmail.com>
2025-07-27 00:29:36 -0400

    Update 05_Py_Transportation_Citibikes_FactTrips.py

--- dimfact/05_Py_Transportation_Citibikes_FactTrips.py
+++ dimfact/05_Py_Transportation_Citibikes_FactTrips.py
@@ -1,27 +1,9 @@
-# Fabric notebook source
-
-# METADATA ********************
-
-# META {
-# META   "kernel_info": {
-# META     "name": "synapse_pyspark"
-# META   },
-# META   "dependencies": {
-# META     "lakehouse": {
-# META       "default_lakehouse": "e8b06649-cbdc-4b98-89b9-ad244d20a675",
-# META       "default_lakehouse_name": "dataml_lakehouse",
-# META       "default_lakehouse_workspace_id": "eda47f07-ebf2-41e7-8215-a7c5aa53a9d8"
-# META     }
-# META   }
-# META }
-
-# CELL ********************
+# Author: Patrick Okare
 
 facttrip_df = spark.sql("""
-    
-   SELECT 
+SELECT 
     ROW_NUMBER() OVER (ORDER BY T.RideID) + 
-        COALESCE((SELECT MAX(F.TripKey) FROM citibikes_gold.facttrip F), 0) AS TripKey, -- Generate Surrogate Key
+        COALESCE((SELECT MAX(F.TripKey) FROM citibikes_gold.facttrip F), 0) AS TripKey,
     COALESCE(T.RideID, 'Unknown') AS RideID,
     COALESCE(S.StationKey, -1) AS StartStationKey,
     COALESCE(ES.StationKey, -1) AS EndStationKey,
@@ -74,97 +56,62 @@ WHERE NOT EXISTS (
       AND tgt.RiderKey = COALESCE(R.RiderKey, -1)
       AND tgt.BikeKey = COALESCE(B.BikeKey, -1)
       AND tgt.StartDateKey = COALESCE(D.Date_ID, -1)
-      )
+)
 """)
 
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-# Create a temporary view
 facttrip_df.createOrReplaceTempView("vw_facttrip")
 
-# METADATA ********************
 
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
+MERGE INTO citibikes_gold.facttrip AS tgt
+USING vw_facttrip AS src
+ON tgt.RideID = src.RideID
+AND tgt.StartStationKey = src.StartStationKey
+AND tgt.EndStationKey = src.EndStationKey
+AND tgt.RiderKey = src.RiderKey
+AND tgt.BikeKey = src.BikeKey
+AND tgt.StartDateKey = src.StartDateKey
 
-# CELL ********************
+WHEN MATCHED AND (
+        src.HASH_ID <> tgt.HASH_ID
+        OR src.FileName <> tgt.FileName
+    ) THEN
+  UPDATE SET
+    tgt.TripDurationMinutes = src.TripDurationMinutes,
+    tgt.DistanceTraveled = src.DistanceTraveled,
+    tgt.TimeOfDay = src.TimeOfDay,
+    tgt.FileName = src.FileName,
+    tgt.SourceSystem = src.SourceSystem,
+    tgt.UpdatedDate = current_timestamp(),
+    tgt.HASH_ID = src.HASH_ID
 
-# MAGIC %%sql
-# MAGIC MERGE INTO citibikes_gold.facttrip AS tgt
-# MAGIC USING vw_facttrip AS src
-# MAGIC ON tgt.RideID = src.RideID
-# MAGIC AND tgt.StartStationKey = src.StartStationKey
-# MAGIC AND tgt.EndStationKey = src.EndStationKey
-# MAGIC AND tgt.RiderKey = src.RiderKey
-# MAGIC AND tgt.BikeKey = src.BikeKey
-# MAGIC AND tgt.StartDateKey = src.StartDateKey
-# MAGIC 
-# MAGIC WHEN MATCHED AND (
-# MAGIC         src.HASH_ID <> tgt.HASH_ID -- Detect changes based on the HASH_ID
-# MAGIC         OR src.FileName <> tgt.FileName -- Ensure temporal accuracy
-# MAGIC     ) THEN
-# MAGIC   UPDATE SET
-# MAGIC     tgt.TripDurationMinutes = src.TripDurationMinutes,
-# MAGIC     tgt.DistanceTraveled = src.DistanceTraveled,
-# MAGIC     tgt.TimeOfDay = src.TimeOfDay,
-# MAGIC     tgt.FileName = src.FileName,
-# MAGIC     tgt.SourceSystem = src.SourceSystem,
-# MAGIC     tgt.UpdatedDate = current_timestamp(),
-# MAGIC     tgt.HASH_ID = src.HASH_ID
-# MAGIC 
-# MAGIC WHEN NOT MATCHED THEN
-# MAGIC   INSERT (
-# MAGIC     TripKey, RideID, StartStationKey, EndStationKey, RiderKey, BikeKey, StartDateKey, 
-# MAGIC     TripDurationMinutes, DistanceTraveled, TimeOfDay, FileName, SourceSystem, InsertedDate, UpdatedDate, HASH_ID
-# MAGIC   )
-# MAGIC   VALUES (
-# MAGIC     src.TripKey, src.RideID, src.StartStationKey, src.EndStationKey, src.RiderKey, src.BikeKey, src.StartDateKey, 
-# MAGIC     src.TripDurationMinutes, src.DistanceTraveled, src.TimeOfDay, src.FileName, src.SourceSystem, src.IngestionDate, NULL, src.HASH_ID
-# MAGIC   );
+WHEN NOT MATCHED THEN
+  INSERT (
+    TripKey, RideID, StartStationKey, EndStationKey, RiderKey, BikeKey, StartDateKey, 
+    TripDurationMinutes, DistanceTraveled, TimeOfDay, FileName, SourceSystem, InsertedDate, UpdatedDate, HASH_ID
+  )
+  VALUES (
+    src.TripKey, src.RideID, src.StartStationKey, src.EndStationKey, src.RiderKey, src.BikeKey, src.StartDateKey, 
+    src.TripDurationMinutes, src.DistanceTraveled, src.TimeOfDay, src.FileName, src.SourceSystem, src.IngestionDate, NULL, src.HASH_ID
+  )
 
-# METADATA ********************
+CREATE TABLE IF NOT EXISTS citibikes_gold.facttrip
+(
+    TripKey BIGINT NOT NULL,
+    RideID STRING NOT NULL,
+    StartStationKey BIGINT NOT NULL,
+    EndStationKey BIGINT NOT NULL,
+    RiderKey BIGINT NOT NULL,
+    BikeKey BIGINT NOT NULL,
+    StartDateKey BIGINT NOT NULL,
 
-# META {
-# META   "language": "sparksql",
-# META   "language_group": "synapse_pyspark"
-# META }
+    TripDurationMinutes DOUBLE NOT NULL,
+    DistanceTraveled DOUBLE NULL,
+    TimeOfDay STRING,
+    FileName STRING NOT NULL,
+    SourceSystem STRING,
+    InsertedDate TIMESTAMP NOT NULL,
+    UpdatedDate TIMESTAMP,
+    HASH_ID STRING NOT NULL
+)
+USING DELTA
 
-# CELL ********************
-
-# MAGIC %%sql
-# MAGIC CREATE TABLE citibikes_gold.facttrip
-# MAGIC (
-# MAGIC     TripKey BIGINT NOT NULL, -- Surrogate Key
-# MAGIC     RideID STRING NOT NULL, -- Primary Key
-# MAGIC     StartStationKey BIGINT NOT NULL, -- Foreign Key to DimStation
-# MAGIC     EndStationKey BIGINT NOT NULL, -- Foreign Key to DimStation
-# MAGIC     RiderKey BIGINT NOT NULL, -- Foreign Key to DimRider
-# MAGIC     BikeKey BIGINT NOT NULL, -- Foreign Key to DimBike
-# MAGIC     StartDateKey BIGINT NOT NULL, -- Foreign Key to Date Dimension
-# MAGIC    
-# MAGIC     TripDurationMinutes DOUBLE NOT NULL, -- Trip duration in minutes
-# MAGIC     DistanceTraveled DOUBLE NULL, -- Calculated distance using lat/long
-# MAGIC     TimeOfDay STRING, -- Time of day classification
-# MAGIC     FileName STRING NOT NULL,
-# MAGIC     SourceSystem STRING, -- Source system for lineage
-# MAGIC     InsertedDate TIMESTAMP NOT NULL, -- Date the record was inserted
-# MAGIC     UpdatedDate TIMESTAMP, -- Date the record was last updated
-# MAGIC     HASH_ID STRING NOT NULL -- Hash value for deduplication and tracking changes
-# MAGIC )
-# MAGIC USING DELTA;
-
-# METADATA ********************
-
-# META {
-# META   "language": "sparksql",
-# META   "language_group": "synapse_pyspark"
-# META }
